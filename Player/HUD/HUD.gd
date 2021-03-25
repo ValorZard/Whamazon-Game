652650e8fca0fe19ad90e20f0796b76e992b4f51
janelleleavins <jaynie.r.lea@gmail.com>
2021-03-24 18:14:45 -0700

    hud stuff

--- Player/HUD/HUD.gd
+++ Player/HUD/HUD.gd
@@ -1,9 +1,16 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var health_label = $Overlay/HBoxContainer/VBoxContainer/Health/HealthLabel
+onready var bar = $Overlay/HBoxContainer/VBoxContainer/Health/Gauge/Health
+onready var tween = $Overlay/Tween
+onready var speed_label = $Overlay/HBoxContainer/VBoxContainer/SpeedLabel
+onready var timer_label = $Overlay/HBoxContainer/TextDisplay
+
+var ms = 0
+var s = 0
+var m = 0
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,8 +18,10 @@ func _ready():
 	pass # Replace with function body.
 
 func update_labels():
-	$CanvasLayer/SpeedLabel.text = "Speed:" + str(-(get_parent().horizontal_speed))
-	$CanvasLayer/HealthLabel.text = "Health:" + str(get_parent().health)
+	speed_label.text = "SPEED:" + str(-(get_parent().horizontal_speed))
+	health_label.text = "HP:" + str(get_parent().health)
+	
+	bar.value = get_parent().health * 10
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
