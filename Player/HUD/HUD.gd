f4c4798b8beeb080252ccf1081d71d4c849d0fcf
janelleleavins <jaynie.r.lea@gmail.com>
2021-03-26 17:16:56 -0700

    added overlay assets

--- Player/HUD/HUD.gd
+++ Player/HUD/HUD.gd
@@ -1,13 +1,14 @@
 extends Control
 
-onready var health_label = $Overlay/HBoxContainer/VBoxContainer/Health/HealthLabel
-onready var bp_bar = $Overlay/HBoxContainer3/VBoxContainer/BPGaugeSped/Node2D/BPFill
-onready var tween = $Overlay/Tween
-onready var speed_label = $Overlay/HBoxContainer3/SpeedLabel
-onready var timer_label = $Overlay/HBoxContainer2/VBoxContainer/Timer
+#onready var health_label = $Overlay/HBoxContainer/VBoxContainer/Health/HealthLabel
+onready var bp_bar = $BPGaugeSped/Node2D/BPFill
+onready var tween = $Tween
+onready var speed_label = $SpeedLabel
+onready var timer_label = $Timer
+onready var driver_icon = $DriverIcon
+onready var needle = $BPGaugeSped/Needle
 
-
-var timer : int = 0
+var timer : float = 0
 
 
 
@@ -17,15 +18,23 @@ func _ready():
 
 func _process(delta):
 	timer += delta
-	timer_label.set_text(str(floor(timer / 60)).pad_zeros(2) + ":" + str(stepify(fmod(timer, 60.0), 0.01)).pad_zeros(2))
+	timer_label.set_text(("%02d" % floor(timer / 60)) + ":" + ("%2.2f" % timer).pad_zeros(2))
 
 func update_speed():
 	speed_label.text = "SPEED:" + str(-(get_parent().horizontal_speed))
+	needle.rect_rotation = abs(get_parent().horizontal_speed)/30.0*280.0
 	
-func update_health(new_health):
-	health_label.text = "PACKAGE HEALTH:" + str(new_health)
-	#tween.interpolate_property(bar, 'value', bar.value, new_health, .1)
-	#tween.start()
+func update_health(updated_health):
+	#health_label.text = "PACKAGE HEALTH:" + str(updated_health)
+	tween.interpolate_property(bp_bar, 'value', bp_bar.value, 100-updated_health, .2)
+	tween.start()
+	
+	if updated_health > 70:
+		driver_icon.texture = load("res://Assets/Overlay/portrait1.png")
+	elif updated_health < 70 and updated_health > 30:
+		driver_icon.texture = load("res://Assets/Overlay/portrait2.png")
+	elif updated_health < 30:
+		driver_icon.texture = load("res://Assets/Overlay/portrait3.png")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
