a92f8a5feba503c45703bc62067c789a50615a96
Srayan Jana <valorzard.kiko@outlook.com>
2021-03-26 18:48:47 -0700

    commit better car stuff

--- Obstacles/AICar.gd
+++ Obstacles/AICar.gd
@@ -5,13 +5,23 @@ extends VehicleBody
 # var a = 2
 # var b = "text"
 
+var car_mesh : MeshInstance
+
+var engine_power : float = -42
+var engine_range : float = 10
+
+var steer_range : float = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	engine_force = -42
+	engine_force = -engine_power
+	car_mesh = get_node("SM_TCar_01/RootNode/SM_TCar_01")
+	# car_mesh.material_override.albedo_color = Color.from_hsv(rand_range(0.0, 6.0), 1.0, 1.0)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	engine_force = rand_range(engine_power - engine_range, engine_power + engine_range)
+	steering = 0
+	pass
